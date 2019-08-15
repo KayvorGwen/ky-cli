d59ede889eb5f6d86a7f5d20717abe428d8b71ee
kayvor <598826424@qq.com>
2019-08-15 14:30:56 +0800

    Revert "update code"
    This reverts commit 13d46165007079b42ff8c73941c74753883de532.

--- README.md
+++ README.md
@@ -1,19 +1,2 @@
 # ky-cli
-
-> 一个简陋版的脚手架
-
-## 使用命令
-
-```
-# 添加模板
-$ ky add
-
-# 删除模板
-$ ky delete
-
-# 查看模板
-$ ky list
-
-# 下载模板
-$ ky init <tpl-name> <app-name>
-```
+一个简陋版的脚手架
--- command/list.js
+++
@@ -1,7 +0,0 @@
-// output the template list
-const path = require('path')
-module.exports = () => {
-    const tplObj = require(`${__dirname}/../templates`)
-    console.log(JSON.stringify(tplObj, null, 4))
-    process.exit(1)
-}
